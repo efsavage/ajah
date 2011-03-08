cc7a15a7f2f2a86ef33969b1ca0fba883b218656
Eric Savage <code@efsavage.com>
2011-03-08 16:59:52 -0500

    import

---
+++ ajah-spring-mvc/src/main/java/com/ajah/spring/mvc/AjahDispatcherServlet.java
@@ -0,0 +1,15 @@
+package com.ajah.spring.mvc;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * Entry point for SpringMVC webapps.
+ * 
+ * @author Eric F. Savage <code@efsavage.com>
+ * 
+ */
+public class AjahDispatcherServlet extends DispatcherServlet {
+
+	private static final long serialVersionUID = 2230373751066084914L;
+
+}
---
+++ ajah-spring-mvc/src/main/java/com/ajah/spring/mvc/package-info.java
@@ -0,0 +1,7 @@
+/**
+The Ajah Spring MVC Library provides support for Spring-MVC-based webapps.
+
+@author Eric F. Savage
+@version 1.0
+ */
+package com.ajah.spring.mvc;
