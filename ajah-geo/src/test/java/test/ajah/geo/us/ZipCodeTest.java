aecffa6a1bae4ddcb7db0e4b1d49a4a7b98763bc
Eric Savage <code@efsavage.com>
2011-03-08 14:21:09 -0500

    license and project info

--- ajah-geo/src/test/java/test/ajah/geo/us/ZipCodeTest.java
+++ ajah-geo/src/test/java/test/ajah/geo/us/ZipCodeTest.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2011 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package test.ajah.geo.us;
 
 import org.junit.Assert;
@@ -68,7 +83,7 @@ public class ZipCodeTest {
 	/**
 	 * Test too short
 	 */
-	@Test(expected=IllegalZipCodeFormatException.class)
+	@Test(expected = IllegalZipCodeFormatException.class)
 	public void tooShort() {
 		ZipCode test = new ZipCode();
 		test.setZip(null);
@@ -79,7 +94,7 @@ public class ZipCodeTest {
 	/**
 	 * Test too long
 	 */
-	@Test(expected=IllegalZipCodeFormatException.class)
+	@Test(expected = IllegalZipCodeFormatException.class)
 	public void tooLong() {
 		ZipCode test = new ZipCode();
 		test.setZip(null);
@@ -90,7 +105,7 @@ public class ZipCodeTest {
 	/**
 	 * Test alpha chars
 	 */
-	@Test(expected=IllegalZipCodeFormatException.class)
+	@Test(expected = IllegalZipCodeFormatException.class)
 	public void alphaChars() {
 		ZipCode test = new ZipCode();
 		test.setZip(null);
@@ -101,7 +116,7 @@ public class ZipCodeTest {
 	/**
 	 * Test alphanumeric chars
 	 */
-	@Test(expected=IllegalZipCodeFormatException.class)
+	@Test(expected = IllegalZipCodeFormatException.class)
 	public void alphaNumericChars() {
 		ZipCode test = new ZipCode();
 		test.setZip(null);
