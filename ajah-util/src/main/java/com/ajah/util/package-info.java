163a22160426d02ab1e7ff70724158886a2a2cca
Eric Savage <code@efsavage.com>
2011-03-08 16:01:00 -0500

    comments

---
+++ ajah-util/src/main/java/com/ajah/util/package-info.java
@@ -0,0 +1,7 @@
+/**
+The Ajah Utils Library provides a place for the most basic and common utilities.  This code uses very few dependencies and should be suitable for inclusion in any other projects.
+
+@author Eric F. Savage
+@version 1.0
+ */
+package com.ajah.util;
