f4284ee7e6d6ffd9a84bf2c74ce8f5c90cf482e8
Eric Savage <code@efsavage.com>
2011-03-08 12:40:40 -0500

    more importing

---
+++ ajah-geo/src/main/java/com/ajah/geo/address/PostalCode.java
@@ -0,0 +1,11 @@
+package com.ajah.geo.address;
+
+/**
+ * Marker interface for use in addresses.
+ * 
+ * @author Eric F. Savage <code@efsavage.com>
+ * 
+ */
+public interface PostalCode {
+	// Empty
+}
