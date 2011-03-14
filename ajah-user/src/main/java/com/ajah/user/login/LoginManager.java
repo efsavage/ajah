d594d2eeebacdc779850aa4f501536fe13868b5a
Eric Savage <code@efsavage.com>
2011-03-14 13:51:42 -0400

    login by token

--- ajah-user/src/main/java/com/ajah/user/login/LoginManager.java
+++ ajah-user/src/main/java/com/ajah/user/login/LoginManager.java
@@ -26,6 +26,8 @@ import com.ajah.user.AuthenicationFailureException;
 import com.ajah.user.User;
 import com.ajah.user.UserNotFoundException;
 import com.ajah.user.data.UserManager;
+import com.ajah.util.crypto.Crypto;
+import com.ajah.util.crypto.HmacSha1Password;
 import com.ajah.util.crypto.Password;
 
 /**
@@ -58,7 +60,7 @@ public class LogInManager {
 	 * @return Login record, will never return null.
 	 */
 	public LogIn login(String username, Password password, String ip, LogInSource source, LogInType type) {
-		log.fine("Login attempt for: " + username);
+		log.fine("Login by user/pass attempt for: " + username);
 		LogIn login = new LogIn();
 		login.setIp(ip);
 		login.setCreated(new Date());
@@ -83,4 +85,44 @@ public class LogInManager {
 		return login;
 	}
 
+	/**
+	 * Returns token value for a user, that can be used to authenticate later.
+	 * The actual scheme of token generation should be encapsulated fully here
+	 * so it can be changed. Currently the only available scheme is to encrypt
+	 * the username and password hash.
+	 * 
+	 * Note: Tokens should always expire when a user changes their password.
+	 * 
+	 * @param user
+	 *            The user to generate a token for.
+	 * @param password
+	 *            Password object for user to include in the token.
+	 * @return Token that can be used to authenticate.
+	 */
+	public String getTokenValue(User user, Password password) {
+		return Crypto.toAES(user.getUsername() + "|" + password.toString());
+	}
+
+	/**
+	 * Logs a user in by token value. Common usage would be to store token in a
+	 * cookie.
+	 * 
+	 * @param token
+	 *            Token value
+	 * @param ip
+	 *            IP of requesting user
+	 * @param source
+	 *            Source of login attempt
+	 * @param type
+	 *            Type of login attempt
+	 * @return Login record, will never be null.
+	 */
+	public LogIn loginByToken(String token, String ip, LogInSource source, LogInType type) {
+		log.fine("Login by token attempt for: " + token);
+		String decrypted = Crypto.fromAES(token);
+		log.fine("token contents: " + decrypted);
+		String username = decrypted.split("\\|")[0];
+		Password password = new HmacSha1Password(decrypted.split("\\|")[1], true);
+		return login(username, password, ip, source, type);
+	}
 }
