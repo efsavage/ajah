2c344d9ea6ed087bf9ff791660632dbb31482111
Eric Savage <code@efsavage.com>
2011-03-14 11:22:57 -0400

    add aes encryption methods

--- ajah-util/src/main/java/com/ajah/util/crypto/Crypto.java
+++ ajah-util/src/main/java/com/ajah/util/crypto/Crypto.java
@@ -18,10 +18,16 @@ package com.ajah.util.crypto;
 import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
 import java.security.SecureRandom;
+import java.security.Security;
 import java.util.Scanner;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -75,14 +81,96 @@ public class Crypto {
 	}
 
 	/**
+	 * Encodes a value in aes and returns the hexadecimal encoded version of the
+	 * encrypted data.
+	 * 
+	 * @param secret
+	 *            Data to encrypt.
+	 * @return Hexadecimal encoded version of the encrypted data.
+	 * @throws UnsupportedOperationException
+	 *             If there is a cryptographic error.
+	 */
+	public static String toAES(String secret) {
+		String keyString = Config.i.get("crypto.key.aes", null);
+		SecretKeySpec skeySpec = new SecretKeySpec(new BigInteger(keyString, 16).toByteArray(), "AES");
+		try {
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+			byte[] encrypted = cipher.doFinal(secret.getBytes());
+			return new BigInteger(encrypted).toString(16);
+		} catch (InvalidKeyException e) {
+			throw new UnsupportedOperationException(e);
+		} catch (IllegalBlockSizeException e) {
+			throw new UnsupportedOperationException(e);
+		} catch (BadPaddingException e) {
+			throw new UnsupportedOperationException(e);
+		} catch (NoSuchAlgorithmException e) {
+			throw new UnsupportedOperationException(e);
+		} catch (NoSuchPaddingException e) {
+			throw new UnsupportedOperationException(e);
+		}
+
+	}
+
+	/**
+	 * Accepts a hexadecimal encoded version of the encrypted data and decrypts
+	 * it.
+	 * 
+	 * @param encrypted
+	 *            hexadecimal encoded version of the encrypted data
+	 * @return Decrypted version.
+	 * @throws UnsupportedOperationException
+	 *             If there is a cryptographic error.
+	 */
+	public static String fromAES(String encrypted) {
+		String keyString = Config.i.get("crypto.key.aes", null);
+		SecretKeySpec skeySpec = new SecretKeySpec(new BigInteger(keyString, 16).toByteArray(), "AES");
+		try {
+			Cipher cipher = Cipher.getInstance("AES");
+
+			cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+			return new String(cipher.doFinal(new BigInteger(encrypted, 16).toByteArray()));
+		} catch (InvalidKeyException e) {
+			throw new UnsupportedOperationException(e);
+		} catch (IllegalBlockSizeException e) {
+			throw new UnsupportedOperationException(e);
+		} catch (BadPaddingException e) {
+			throw new UnsupportedOperationException(e);
+		} catch (NoSuchAlgorithmException e) {
+			throw new UnsupportedOperationException(e);
+		} catch (NoSuchPaddingException e) {
+			throw new UnsupportedOperationException(e);
+		}
+	}
+
+	/**
 	 * Command line utility for testing passwords and such.
 	 * 
 	 * @param args
 	 *            No args, program is interactive.
+	 * @throws Exception
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			return;
+		}
+		if ("list".equals(args[0])) {
+			listProviders();
+			return;
+		} else if ("keygen".equals(args[0])) {
+			keyGen();
+		} else if ("to-aes".equals(args[0])) {
+			String secret = "turtle";
+			String encrypted = toAES(secret);
+			System.out.println(encrypted);
+			String decrypted = fromAES(encrypted);
+			System.out.println(decrypted);
+		}
+	}
+
+	private static void keyGen() {
 		SecureRandom sr = new SecureRandom();
-		byte[] keyBytes = new byte[20];
+		byte[] keyBytes = new byte[128];
 		sr.nextBytes(keyBytes);
 		System.out.println(new BigInteger(keyBytes).toString(16));
 
@@ -93,6 +181,16 @@ public class Crypto {
 			System.out.print("Secret: ");
 			String secret = scanner.nextLine();
 			System.out.print(getHmacSha1Hex(secret));
+		}
+	}
+
+	private static void listProviders() {
+		System.out.println("Supported providers:");
+		for (Provider provider : Security.getProviders()) {
+			System.out.println("\t" + provider);
+			for (Provider.Service service : provider.getServices()) {
+				System.out.println("\t\t" + service.getAlgorithm());
+			}
 		}
 	}
 
