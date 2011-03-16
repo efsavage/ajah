46bbda391eca4cf5fd2def713d444f3e892e4aa6
Eric Savage <code@efsavage.com>
2011-03-16 19:37:19 -0400

    placeholders

---
+++ ajah-spring/src/main/java/com/ajah/spring/package-info.java
@@ -0,0 +1,9 @@
+/**
+The Ajah Spring Library provides support for Spring-based applications.
+
+Spring-MVC-specific code should go in the spring-mvc library.
+
+@author Eric F. Savage
+@version 1.0
+ */
+package com.ajah.spring;
