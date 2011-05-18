6f646632bebfcc7eb37a8b550a868e067ddddb8d
Eric Savage <code@efsavage.com>
2011-05-17 23:46:04 -0400

    Added some reflection utilities

---
+++ ajah-util/src/main/java/com/ajah/util/reflect/IntrospectionUtils.java
@@ -0,0 +1,135 @@
+/*
+ *  Copyright 2011 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.ajah.util.reflect;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+
+import com.ajah.util.AjahUtils;
+import com.ajah.util.FromStringable;
+import com.ajah.util.Identifiable;
+import com.ajah.util.ToStringable;
+
+/**
+ * Java reflection utilities.
+ * 
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ * 
+ */
+public class IntrospectionUtils {
+
+	/**
+	 * Checks to see if the field's type is a {@link String}.
+	 * 
+	 * @param field
+	 *            The field to check the type of, required.
+	 * @return true if the field's type is a String
+	 */
+	public static boolean isString(Field field) {
+		AjahUtils.requireParam(field, "field");
+		return String.class.isAssignableFrom(field.getType());
+	}
+
+	/**
+	 * Checks to see if the field's type is a {@link Date}.
+	 * 
+	 * @param field
+	 *            The field to check the type of, required.
+	 * @return true if the field's type is a Date
+	 */
+	public static boolean isDate(Field field) {
+		AjahUtils.requireParam(field, "field");
+		return Date.class.isAssignableFrom(field.getType());
+	}
+
+	/**
+	 * Checks to see if the field's type implements {@link ToStringable}.
+	 * 
+	 * @param field
+	 *            The field to check the type of, required.
+	 * @return true if the field's type implements ToStringable
+	 */
+	public static boolean isToStringable(Field field) {
+		AjahUtils.requireParam(field, "field");
+		return ToStringable.class.isAssignableFrom(field.getType());
+	}
+
+	/**
+	 * Checks to see if the field's type implements {@link Identifiable}.
+	 * 
+	 * @param field
+	 *            The field to check the type of, required.
+	 * @return true if the field's type implements Identifiable
+	 */
+	public static boolean isIdentifiable(Field field) {
+		AjahUtils.requireParam(field, "field");
+		return Identifiable.class.isAssignableFrom(field.getType());
+	}
+
+	/**
+	 * Checks to see if the field's type is an {@link Enum}.
+	 * 
+	 * Note: There is an easier method that does this already,
+	 * {@link Class#isEnum()}, this method is just included here for
+	 * consistency.
+	 * 
+	 * @param field
+	 *            The field to check the type of, required.
+	 * @return true if the field's type is an Enum
+	 */
+	public static boolean isEnum(Field field) {
+		AjahUtils.requireParam(field, "field");
+		return field.getType().isEnum();
+	}
+
+	/**
+	 * Checks to see if the field's type implements {@link FromStringable}.
+	 * 
+	 * @param field
+	 *            The field to check the type of, required.
+	 * @return true if the field's type implements FromStringable
+	 */
+	public static boolean isFromStringable(Field field) {
+		AjahUtils.requireParam(field, "field");
+		return FromStringable.class.isAssignableFrom(field.getType());
+	}
+
+	/**
+	 * Checks to see if the field's type is an int.
+	 * 
+	 * @param field
+	 *            The field to check the type of, required.
+	 * @return true if the field's type is an int
+	 */
+	public static boolean isInt(Field field) {
+		AjahUtils.requireParam(field, "field");
+		return int.class.isAssignableFrom(field.getType());
+	}
+
+	/**
+	 * Checks to see if the field's type implements {@link Identifiable} and is
+	 * an {@link Enum}.
+	 * 
+	 * @param field
+	 *            The field to check the type of, required.
+	 * @return true if the field's type implements Identifiable and is an Enum
+	 */
+	public static boolean isIdentifiableEnum(Field field) {
+		return isIdentifiable(field) && isEnum(field);
+	}
+
+}
---
+++ ajah-util/src/main/java/com/ajah/util/reflect/ReflectionUtils.java
@@ -0,0 +1,112 @@
+/*
+ *  Copyright 2011 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.ajah.util.reflect;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.ajah.util.AjahUtils;
+import com.ajah.util.Identifiable;
+
+/**
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ * 
+ */
+public class ReflectionUtils {
+
+	private static final Logger log = Logger.getLogger(ReflectionUtils.class.getName());
+
+	/**
+	 * Executes the read method for a property descriptor. If any errors occur
+	 * they are logged, and will result in null return value.
+	 * 
+	 * @param instance
+	 * @param propertyDescriptor
+	 * @return The value of the objects read method for the property specified
+	 *         (which may be null), or null if an error occurs.
+	 */
+	public static Object propGetSafe(Object instance, PropertyDescriptor propertyDescriptor) {
+		AjahUtils.requireParam(instance, "instance");
+		AjahUtils.requireParam(propertyDescriptor, "propertyDescriptor");
+		try {
+			Method getter = propertyDescriptor.getReadMethod();
+			if (getter != null) {
+				return getter.invoke(instance);
+			}
+			log.log(Level.SEVERE, "No read method found for " + propertyDescriptor.getName() + " on class " + instance.getClass().getName());
+		} catch (RuntimeException e) {
+			log.log(Level.SEVERE, propertyDescriptor.getName() + ": " + e.getMessage(), e);
+		} catch (IllegalAccessException e) {
+			log.log(Level.SEVERE, propertyDescriptor.getName() + ": " + e.getMessage(), e);
+		} catch (InvocationTargetException e) {
+			log.log(Level.SEVERE, propertyDescriptor.getName() + ": " + e.getMessage(), e);
+		}
+		return null;
+	}
+
+	/**
+	 * Executes the read method for a property descriptor, and returns as a
+	 * {@link Date}. If any errors occur they are logged, and will result in
+	 * null return value.
+	 * 
+	 * @param instance
+	 *            The instance on which the Property Descriptor is.
+	 * @param propertyDescriptor
+	 *            The property descriptor for the property sought.
+	 * @return The value of the objects read method for the property specified
+	 *         (which may be null), or null if an error occurs.
+	 */
+	public static Date propGetDateSafe(Object instance, PropertyDescriptor propertyDescriptor) {
+		Object value = ReflectionUtils.propGetSafe(instance, propertyDescriptor);
+		if (value == null || !(value instanceof Date)) {
+			return null;
+		}
+		return (Date) value;
+	}
+
+	/**
+	 * Finds the enum where the getId() value matches the value of the field for
+	 * this particular object.
+	 * 
+	 * @param field
+	 *            The field to get the value of.
+	 * @param value
+	 *            The value of the field.
+	 * @return The value, null if the field value is null, also null on error.
+	 */
+	public static Object findEnumById(Field field, Object value) {
+		if (!IntrospectionUtils.isIdentifiableEnum(field)) {
+			throw new IllegalArgumentException("Field " + field.getName() + " is not an Identifiable enum");
+		}
+		if (value == null) {
+			return null;
+		}
+		Object[] elements = field.getType().getEnumConstants();
+		for (Object element : elements) {
+			if (((Identifiable<?>) element).getId().equals(value)) {
+				return element;
+			}
+		}
+		return null;
+	}
+
+}
