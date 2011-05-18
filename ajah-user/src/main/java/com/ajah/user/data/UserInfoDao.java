c52f524d3c9bb1b0365c83da6b4d0bbd2166aed8
Eric Savage <code@efsavage.com>
2011-05-17 23:47:57 -0400

    update to return rows affected

--- ajah-user/src/main/java/com/ajah/user/data/UserInfoDao.java
+++ ajah-user/src/main/java/com/ajah/user/data/UserInfoDao.java
@@ -39,12 +39,12 @@ public class UserInfoDao extends AbstractAjahDao<UserId, UserInfo> {
 	 * @param userInfo
 	 *            UserInfo entity to insert, required.
 	 */
-	public void insert(UserInfo userInfo) {
+	public int insert(UserInfo userInfo) {
 		AjahUtils.requireParam(userInfo, "userInfo");
 		AjahUtils.requireParam(this.jdbcTemplate, "this.jdbcTemplate");
-		this.jdbcTemplate
+		return this.jdbcTemplate
 				.update("INSERT INTO user_info (user_id, first_name, middle_name, last_name, birth_day, birth_month, birth_year, primary_email_id) VALUES (?,?,?,?,?,?,?,?)",
-						new Object[] { userInfo.getUserId().getId(), userInfo.getFirstName(), userInfo.getMiddleName(), userInfo.getLastName(),
+						new Object[] { userInfo.getId().getId(), userInfo.getFirstName(), userInfo.getMiddleName(), userInfo.getLastName(),
 								userInfo.getBirthDay(), userInfo.getBirthMonth() == null ? null : Integer.valueOf(userInfo.getBirthMonth().getId()),
 								userInfo.getBirthYear(), userInfo.getPrimaryEmailId().getId() });
 	}
