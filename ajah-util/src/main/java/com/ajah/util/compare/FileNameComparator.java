a24f01f6231c45f8a3169322b2a20e726c823cbc
Eric Savage <code@efsavage.com>
2011-08-23 08:47:01 -0400

    Compare files based on names

---
+++ ajah-util/src/main/java/com/ajah/util/compare/FileNameComparator.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2011 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.ajah.util.compare;
+
+import java.io.File;
+import java.util.Comparator;
+
+/**
+ * Compares two files by file size, with the smaller file first.
+ * 
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ * 
+ */
+public class FileNameComparator implements Comparator<File> {
+
+	private static final FileNameComparator instance = new FileNameComparator();
+
+	/**
+	 * Returns singleton instance of this comparator.
+	 * 
+	 * @return Singleton instance of this comparator.
+	 */
+	public static Comparator<? super File> getInstance() {
+		return instance;
+	}
+
+	/**
+	 * @see CompareUtils#compare(long, long)
+	 * @throws IllegalArgumentException
+	 *             If Both values are null, as this means it cannot be passed
+	 *             along to a comparator even though they are "equal".
+	 */
+	@Override
+	public int compare(File first, File second) {
+		int retVal = CompareUtils.compareNulls(first, second);
+		if (retVal != 0) {
+			System.err.println("Null!");
+			return retVal;
+		}
+		return CompareUtils.compare(first.getName(), second.getName());
+	}
+
+}
