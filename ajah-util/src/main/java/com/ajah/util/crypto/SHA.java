571577e00c5c2f2d154676bcceee015fedde5f22
Eric Savage <code@efsavage.com>
2011-08-29 23:34:15 -0400

    Require param, add comments

--- ajah-util/src/main/java/com/ajah/util/crypto/SHA.java
+++ ajah-util/src/main/java/com/ajah/util/crypto/SHA.java
@@ -5,9 +5,27 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import com.ajah.util.AjahUtils;
+
+/**
+ * Simple Utility class for creating SHA values.
+ * 
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ * 
+ */
 public class SHA {
 
+	/**
+	 * Compute the SHA-1 digest of a string and return it as a hexadecimal
+	 * value.
+	 * 
+	 * @param string
+	 *            The string to hash, required.
+	 * @return THe a hexadecimal value of the string.
+	 */
 	public static String sha1Hex(final String string) {
+		AjahUtils.requireParam(string, "string");
 		try {
 			final MessageDigest md = MessageDigest.getInstance("SHA-1");
 			byte[] bytes = null;
