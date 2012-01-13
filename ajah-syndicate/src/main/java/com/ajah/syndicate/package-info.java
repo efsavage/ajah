18df747bfbd2aeac7d74b529ad472c3158a012f9
Eric Savage <code@efsavage.com>
2012-01-13 14:42:37 -0500

    Comments

--- ajah-syndicate/src/main/java/com/ajah/syndicate/package-info.java
+++ ajah-syndicate/src/main/java/com/ajah/syndicate/package-info.java
@@ -1,6 +1,8 @@
 /**
 The Ajah Syndication Library provides support for feeds, broadcasts, and subscriptions.
 
+Hierarchy is: A {@link com.ajah.syndicate.FeedSource} creates {@link com.ajah.syndicate.Feed}s which contain {@link com.ajah.syndicate.Entry}s.
+ 
 @author Eric F. Savage
 @version 1.0
  */
