9355fa988f3a818b1720d09a46eb3791a3c3b707
Eric Savage <code@efsavage.com>
2012-03-07 16:55:24 -0500

    Throw 404 as exception

---
+++ ajah-spring-mvc/src/main/java/com/ajah/spring/mvc/error/ResourceNotFoundException.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2012 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.ajah.spring.mvc.error;
+
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Throws a 404, as an exception.
+ * 
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception {
+
+	private final HttpServletRequest request;
+
+	/**
+	 * Thrown when a reason can be supplied to the user.
+	 * 
+	 * @param reason
+	 *            A user-viewable reason for this error.
+	 * @param request
+	 *            The request that spawned the error.
+	 */
+	public ResourceNotFoundException(String reason, HttpServletRequest request) {
+		super(reason);
+		this.request = request;
+	}
+
+}
