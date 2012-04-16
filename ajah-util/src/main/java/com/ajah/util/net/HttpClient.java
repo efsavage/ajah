e2a19e304142a85ac2249edb876d0ea97d7ba051
Eric Savage <code@efsavage.com>
2012-04-16 18:09:33 -0400

    cleanup

--- ajah-util/src/main/java/com/ajah/util/net/HttpClient.java
+++ ajah-util/src/main/java/com/ajah/util/net/HttpClient.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2011 Eric F. Savage, code@efsavage.com
+` *  Copyright 2011 Eric F. Savage, code@efsavage.com
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
