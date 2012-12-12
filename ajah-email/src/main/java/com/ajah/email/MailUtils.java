a7803af8dc2853ab79de86b671fa828bb87c7f18
Eric Savage <code@efsavage.com>
2012-12-12 16:00:29 -0500

    Add safeClose for IMAP connection objects

---
+++ ajah-email/src/main/java/com/ajah/email/MailUtils.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright 2012 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.ajah.email;
+
+import java.util.logging.Level;
+
+import javax.mail.MessagingException;
+import javax.mail.Store;
+
+import lombok.extern.java.Log;
+
+import com.sun.mail.imap.IMAPFolder;
+
+/**
+ * Utilities for dealing with Mail
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ */
+@Log
+public class MailUtils {
+
+	/**
+	 * Closes an {@link IMAPFolder}, logging but ignoring any errors that occur
+	 * while doing so.
+	 * 
+	 * @param folder
+	 *            The folder to close.
+	 */
+	public static void safeClose(IMAPFolder folder) {
+		if (folder != null && folder.isOpen()) {
+			try {
+				folder.close(false);
+			} catch (MessagingException e) {
+				log.log(Level.WARNING, e.getMessage(), e);
+			}
+		}
+	}
+
+	/**
+	 * Closes an {@link IMAPFolder}, logging but ignoring any errors that occur
+	 * while doing so.
+	 * 
+	 * @param store
+	 *            The store to close.
+	 */
+	public static void safeClose(Store store) {
+		if (store != null && store.isConnected()) {
+			try {
+				store.close();
+			} catch (MessagingException e) {
+				log.log(Level.WARNING, e.getMessage(), e);
+			}
+		}
+	}
+
+}
