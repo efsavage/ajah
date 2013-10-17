bc712b0786f9a79ad6b268780b4cfe8d842b8f85
Eric Savage <code@efsavage.com>
2013-10-17 16:14:51 -0400

    Mostly complete reader/writer

---
+++ ajah-csv/src/main/java/com/ajah/flatfile/FlatFileReader.java
@@ -0,0 +1,230 @@
+/*
+ *  Copyright 2013 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.ajah.flatfile;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.java.Log;
+
+import com.ajah.util.StringUtils;
+
+/**
+ * Reads a flat data file into a structured iterator.
+ * 
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ */
+@Log
+public class FlatFileReader implements Closeable, Iterable<FlatFileRow>, Iterator<FlatFileRow> {
+
+	private final BufferedReader reader;
+	@Getter
+	private final List<FlatFileColumn> columns = new ArrayList<>();
+	private final Map<String, FlatFileColumn> map = new HashMap<>();
+	private FlatFileRow row = null;
+	@Getter
+	private final FlatFileFormat format;
+	@Getter
+	@Setter
+	private boolean flushEveryLine;
+
+	/**
+	 * Constructs a reader from a file.
+	 * 
+	 * @param format
+	 *            The format of the file.
+	 * @param file
+	 *            The file.
+	 * @throws IOException
+	 *             If the file could not be read.
+	 */
+	public FlatFileReader(FlatFileFormat format, File file) throws IOException {
+		this(format, new BufferedReader(new FileReader(file)));
+	}
+
+	/**
+	 * Constructs a reader from a file.
+	 * 
+	 * @param format
+	 *            The format of the file.
+	 * @param inputStream
+	 *            The input stream for the file.
+	 * @throws IOException
+	 *             If the file could not be read.
+	 */
+	public FlatFileReader(FlatFileFormat format, InputStream inputStream) throws IOException {
+		this(format, new BufferedReader(new InputStreamReader(inputStream)));
+	}
+
+	/**
+	 * Constructs a reader from a file.
+	 * 
+	 * @param format
+	 *            The format of the file.
+	 * @param bufferedReader
+	 *            The buffered reader for the file.
+	 * @throws IOException
+	 *             If the file could not be read.
+	 */
+	public FlatFileReader(FlatFileFormat format, BufferedReader bufferedReader) throws IOException {
+		this.format = format;
+		this.reader = bufferedReader;
+		String header = this.reader.readLine();
+		if (header != null) {
+			createColumns(header);
+		}
+	}
+
+	private void createColumns(String header) {
+		log.fine("Adding columns");
+		switch (this.format) {
+		case CSV: {
+			if (header.contains("\"")) {
+				throw new UnsupportedOperationException("Header contains double quote: " + header);
+			}
+			String[] names = header.split(",");
+			for (String name : names) {
+				addColumn(name);
+			}
+			break;
+		}
+		case TAB: {
+			String[] names = header.split("\t");
+			for (String name : names) {
+				addColumn(name);
+			}
+			break;
+		}
+		default:
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		this.reader.close();
+	}
+
+	@Override
+	public Iterator<FlatFileRow> iterator() {
+		return this;
+	}
+
+	private void addColumn(String name) {
+		if (this.map.get(name) != null) {
+			throw new IllegalArgumentException("Duplicate column name " + name);
+		}
+		FlatFileColumn column = new FlatFileColumn(name, null, false);
+		this.columns.add(column);
+		this.map.put(column.getName(), column);
+		log.fine("Created column \"" + name + "\"");
+	}
+
+	@Override
+	public boolean hasNext() {
+		try {
+			return this.reader.ready();
+		} catch (IOException e) {
+			return false;
+		}
+	}
+
+	@Override
+	public FlatFileRow next() {
+		this.row = new FlatFileRow(this.map);
+		String line;
+		try {
+			line = this.reader.readLine();
+		} catch (IOException e) {
+			return null;
+		}
+		switch (this.format) {
+		case CSV: {
+			int currentPos = 0;
+			int fieldStartPos = 0;
+			boolean inField = false;
+			char[] chars = line.toCharArray();
+			int len = chars.length;
+			for (int i = 0; i < this.columns.size() && currentPos < len; i++) {
+				char c = chars[currentPos];
+				if (c == ',' || c == '"') {
+				} else {
+					if (inField) {
+						fieldStartPos = currentPos;
+						currentPos++;
+						inField = true;
+					}
+
+				}
+			}
+			if (line.contains("\"\"")) {
+				throw new UnsupportedOperationException("Line contains double quote: " + line);
+			}
+			String[] values = line.split(",");
+			for (int i = 0; i < values.length; i++) {
+				if (StringUtils.isBlank(values[i])) {
+					this.row.set(this.columns.get(i).getName(), "");
+				} else if (values[i].charAt(0) == '"' && values[i].charAt(values[i].length() - 1) == '"') {
+					this.row.set(this.columns.get(i).getName(), values[i].substring(1, values[i].length() - 2));
+				} else {
+					this.row.set(this.columns.get(i).getName(), values[i]);
+				}
+			}
+			break;
+		}
+		case TAB: {
+			String[] values = line.split("\t");
+			for (int i = 0; i < values.length; i++) {
+				this.row.set(this.columns.get(i).getName(), values[i]);
+			}
+			break;
+		}
+		default:
+			throw new UnsupportedOperationException();
+		}
+		return this.row;
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * Determines if this reader has a column available.
+	 * 
+	 * @param column
+	 *            The column name to check.
+	 * @return true if the column appears in the file, otherwise false.
+	 */
+	public boolean hasColumn(String column) {
+		return this.map.get(column) != null;
+	}
+
+}
---
+++ ajah-csv/src/main/java/com/ajah/flatfile/FlatFileWriter.java
@@ -0,0 +1,173 @@
+/*
+ *  Copyright 2013 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.ajah.flatfile;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.java.Log;
+
+import com.ajah.util.StringUtils;
+
+/**
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ */
+@Log
+public class FlatFileWriter implements Closeable {
+
+	private final FileWriter writer;
+	private final List<FlatFileColumn> columns = new ArrayList<>();
+	private final Map<String, FlatFileColumn> map = new HashMap<>();
+	private FlatFileRow row = null;
+	@Getter
+	private final FlatFileFormat format;
+	@Getter
+	private final File file;
+	@Getter
+	@Setter
+	private boolean flushEveryLine;
+
+	public FlatFileWriter(FlatFileFormat format, File file) throws IOException {
+		this.format = format;
+		this.file = file;
+		this.writer = new FileWriter(file);
+	}
+
+	public void addColumn(String name, boolean required) {
+		if (this.map.get(name) != null) {
+			throw new IllegalArgumentException("Duplicate column name " + name);
+		}
+		FlatFileColumn column = new FlatFileColumn(name, null, required);
+		this.columns.add(column);
+		this.map.put(column.getName(), column);
+	}
+
+	public void addColumn(String name, String defaultValue) {
+		if (this.map.get(name) != null) {
+			throw new IllegalArgumentException("Duplicate column name " + name);
+		}
+		FlatFileColumn column = new FlatFileColumn(name, defaultValue, false);
+		this.columns.add(column);
+		this.map.put(column.getName(), column);
+	}
+
+	@Override
+	public void close() throws IOException {
+		if (this.row == null) {
+			writeHeader();
+		} else {
+			writeRow();
+		}
+		this.writer.close();
+	}
+
+	public FlatFileRow newRow() throws IOException {
+		if (this.row == null) {
+			writeHeader();
+		} else {
+			writeRow();
+		}
+		this.row = new FlatFileRow(this.map);
+		return this.row;
+	}
+
+	private void writeRow() throws IOException {
+		boolean first = true;
+		for (FlatFileColumn column : this.columns) {
+			String value = this.row.get(column.getName());
+			if (column.isRequired() && StringUtils.isBlank(value)) {
+				throw new IllegalArgumentException("Column is required: " + column.getName());
+			}
+			write(value, first);
+			if (first) {
+				first = false;
+			}
+		}
+		this.writer.write("\r\n");
+		if (this.flushEveryLine) {
+			this.writer.flush();
+		}
+		this.row = null;
+	}
+
+	public void writeHeader() throws IOException {
+		boolean first = true;
+		for (FlatFileColumn column : this.columns) {
+			write(column.getName(), first);
+			if (first) {
+				first = false;
+			}
+		}
+		this.writer.write("\r\n");
+		if (this.flushEveryLine) {
+			this.writer.flush();
+		}
+	}
+
+	private void write(String value, boolean first) throws IOException {
+		switch (this.format) {
+		case CSV:
+			if (!first) {
+				this.writer.write(",");
+			}
+			if (value.contains(",") || value.contains("\"") || value.contains("\r") || value.contains("\n")) {
+				this.writer.write("\"" + value.replaceAll("\"", "\"\"") + "\"");
+			} else {
+				this.writer.write(value);
+			}
+			break;
+		case TAB:
+			if (value != null && value.contains("\t")) {
+				log.log(Level.WARNING, "Writing data containing tabs to a tab-delimited file");
+			}
+			if (!first) {
+				this.writer.write("\t");
+			}
+			this.writer.write(value);
+			break;
+
+		}
+	}
+
+	public void addColumn(String name) {
+		addColumn(name, false);
+	}
+
+	/**
+	 * This is for injecting non-standard content such as an extra header or
+	 * footer row.
+	 * 
+	 * @param string
+	 *            The string to write as-is. Should include any line terminators
+	 *            if they are necessary.
+	 * @throws IOException
+	 *             If the string could not be written.
+	 */
+	public void writeString(String string) throws IOException {
+		this.writer.write(string);
+	}
+
+}
