76ebc9e0f75a3cf49e6825a53c4606e71eb4757c
Eric Savage <code@efsavage.com>
2014-01-11 17:26:21 -0500

    Add EmptyEnumeration

---
+++ ajah-util/src/main/java/com/ajah/lang/EmptyEnumeration.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2013 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.ajah.lang;
+
+import java.util.Enumeration;
+
+/**
+ * An empty {@link Enumeration}, useful as a placeholder.
+ * 
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ * @param <T>
+ *            The enumerated Type.
+ */
+public class EmptyEnumeration<T> implements Enumeration<T> {
+
+	/**
+	 * @see java.util.Enumeration#hasMoreElements()
+	 */
+	@Override
+	public boolean hasMoreElements() {
+		return false;
+	}
+
+	/**
+	 * @see java.util.Enumeration#nextElement()
+	 */
+	@Override
+	public T nextElement() {
+		return null;
+	}
+
+}
