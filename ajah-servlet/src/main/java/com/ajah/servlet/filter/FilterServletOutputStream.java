75262180d1471d06dc8744e2c08d61f84953e2bd
Eric Savage <code@efsavage.com>
2015-07-13 13:13:02 -0400

    Update servlet version

--- ajah-servlet/src/main/java/com/ajah/servlet/filter/FilterServletOutputStream.java
+++ ajah-servlet/src/main/java/com/ajah/servlet/filter/FilterServletOutputStream.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2014-2015 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.ajah.servlet.filter;
 
 import java.io.DataOutputStream;
@@ -5,6 +20,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import javax.servlet.ServletOutputStream;
+import javax.servlet.WriteListener;
 
 /**
  * This is a simple implementation of the abstract {@link ServletOutputStream}.
@@ -53,6 +69,27 @@ public class FilterServletOutputStream extends ServletOutputStream {
 	@Override
 	public void write(final int b) throws IOException {
 		this.stream.write(b);
+	}
+
+	/**
+	 * Always returns true.
+	 * 
+	 * @return Always returns true.
+	 */
+	@Override
+	public boolean isReady() {
+		return true;
+	}
+
+	/**
+	 * Does nothing, exists only for compatibility.
+	 * 
+	 * @param writeListener
+	 *            Ignored.
+	 */
+	@Override
+	public void setWriteListener(WriteListener writeListener) {
+		// Empty
 	}
 
 }
