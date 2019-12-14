af09a1272558992eb35302e5b6100cd9c47309f8
Eric Savage <code@efsavage.com>
2019-12-13 22:54:40 -0500

    Add isBlank method.

--- ajah-csv/src/main/java/com/ajah/flatfile/FlatFileRow.java
+++ ajah-csv/src/main/java/com/ajah/flatfile/FlatFileRow.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2013-2014 Eric F. Savage, code@efsavage.com
+ *  Copyright 2013-2019 Eric F. Savage, code@efsavage.com and Google
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -21,17 +21,17 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.ajah.util.StringUtils;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.java.Log;
 
-import com.ajah.util.StringUtils;
-
 /**
  * Represents a row of a flat-file.
- * 
- * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
- *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ *
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>,
+ * <a href="mailto:code@efsavage.com">code@efsavage.com</a>.
  */
 @Log
 public class FlatFileRow {
@@ -48,21 +48,19 @@ public class FlatFileRow {
 	 * Excel will wrap any field that contains a space or other character with
 	 * double quotes even when saving in tab-delimited format.
 	 */
-	@Getter
-	@Setter
-	private boolean stripWrappedQuotes;
+	@Getter @Setter private boolean stripWrappedQuotes;
 
 	private final int lineNumber;
 
 	/**
 	 * Create an empty row based on a column set.
-	 * 
+	 *
 	 * @param columns
-	 *            The columns in the row.
+	 * 		The columns in the row.
 	 * @param reader
-	 *            The reader that created this row.
+	 * 		The reader that created this row.
 	 * @param lineNumber
-	 *            The line number this row is from.
+	 * 		The line number this row is from.
 	 */
 	public FlatFileRow(final Map<String, FlatFileColumn> columns, final FlatFileReader reader, final int lineNumber) {
 		this.columns = columns;
@@ -72,13 +70,13 @@ public class FlatFileRow {
 
 	/**
 	 * Create an empty row based on a column set.
-	 * 
+	 *
 	 * @param columns
-	 *            The columns in the row.
+	 * 		The columns in the row.
 	 * @param writer
-	 *            The writer that created this row.
+	 * 		The writer that created this row.
 	 * @param lineNumber
-	 *            The line number this row is from.
+	 * 		The line number this row is from.
 	 */
 	public FlatFileRow(final Map<String, FlatFileColumn> columns, final FlatFileWriter writer, final int lineNumber) {
 		this.columns = columns;
@@ -88,11 +86,11 @@ public class FlatFileRow {
 
 	/**
 	 * Fetches the value of a column for this row.
-	 * 
+	 *
 	 * @param name
-	 *            The name of the column.
+	 * 		The name of the column.
 	 * @return The value of the column, or the default value if one is
-	 *         configured.
+	 * configured.
 	 */
 	public String get(final String name) {
 		final FlatFileColumn column = this.columns.get(name);
@@ -116,14 +114,14 @@ public class FlatFileRow {
 
 	/**
 	 * Fetches the value of a column as a BigDecimal.
-	 * 
-	 * @see BigDecimal#BigDecimal(String)
-	 * @see #get(String)
+	 *
 	 * @param column
-	 *            The name of the column
+	 * 		The name of the column
 	 * @return The value, parsed as a BigDecimal. Value is 0 if blank.
 	 * @throws NumberFormatException
-	 *             If the number could not be parsed.
+	 * 		If the number could not be parsed.
+	 * @see BigDecimal#BigDecimal(String)
+	 * @see #get(String)
 	 */
 	public BigDecimal getBigDecimal(final String column) {
 		final String value = get(column);
@@ -135,14 +133,14 @@ public class FlatFileRow {
 
 	/**
 	 * Fetches the value of a column as a double.
-	 * 
-	 * @see Double#parseDouble(String)
-	 * @see #get(String)
+	 *
 	 * @param column
-	 *            The name of the column
+	 * 		The name of the column
 	 * @return The value, parsed as a double. Returns 0 if blank.
 	 * @throws NumberFormatException
-	 *             If the number could not be parsed.
+	 * 		If the number could not be parsed.
+	 * @see Double#parseDouble(String)
+	 * @see #get(String)
 	 */
 	public double getDouble(final String column) {
 		final String value = get(column);
@@ -154,14 +152,14 @@ public class FlatFileRow {
 
 	/**
 	 * Fetches the value of a column as an integer.
-	 * 
-	 * @see Integer#parseInt(String)
-	 * @see #get(String)
+	 *
 	 * @param column
-	 *            The name of the column
+	 * 		The name of the column
 	 * @return The value, parsed as an int. Returns 0 if blank.
 	 * @throws NumberFormatException
-	 *             If the number could not be parsed.
+	 * 		If the number could not be parsed.
+	 * @see Integer#parseInt(String)
+	 * @see #get(String)
 	 */
 	public int getInt(final String column) {
 		final String value = get(column);
@@ -173,15 +171,15 @@ public class FlatFileRow {
 
 	/**
 	 * Fetches the value of a column as an integer with a default value.
-	 * 
+	 *
+	 * @param column
+	 * 		The name of the column
+	 * @param defaultValue
+	 * 		The default value if an integer cannot be parsed.
+	 * @return The value, parsed as an int. Returns the default value if blank
+	 * or if it cannot be parsed as a number.
 	 * @see Integer#parseInt(String)
 	 * @see #get(String)
-	 * @param column
-	 *            The name of the column
-	 * @param defaultValue
-	 *            The default value if an integer cannot be parsed.
-	 * @return The value, parsed as an int. Returns the default value if blank
-	 *         or if it cannot be parsed as a number.
 	 */
 	public int getInt(final String column, final int defaultValue) {
 		final String value = get(column);
@@ -198,7 +196,7 @@ public class FlatFileRow {
 
 	/**
 	 * The reader for this row.
-	 * 
+	 *
 	 * @return the reader that created this row.
 	 */
 	public FlatFileReader getReader() {
@@ -207,7 +205,7 @@ public class FlatFileRow {
 
 	/**
 	 * The writer for this row.
-	 * 
+	 *
 	 * @return the writer that created this row.
 	 */
 	public FlatFileWriter getWriter() {
@@ -216,11 +214,11 @@ public class FlatFileRow {
 
 	/**
 	 * Sets a column to a Date value.
-	 * 
+	 *
 	 * @param name
-	 *            The name of the column.
+	 * 		The name of the column.
 	 * @param date
-	 *            The value to set.
+	 * 		The value to set.
 	 */
 	public void set(final String name, final Date date) {
 		set(name, dateTimeFormat.format(date));
@@ -228,11 +226,11 @@ public class FlatFileRow {
 
 	/**
 	 * Sets a column to a int value.
-	 * 
+	 *
 	 * @param column
-	 *            The name of the column.
+	 * 		The name of the column.
 	 * @param value
-	 *            The value to set.
+	 * 		The value to set.
 	 */
 	public void set(final String column, final int value) {
 		// TODO Store as a number for formatting
@@ -241,11 +239,11 @@ public class FlatFileRow {
 
 	/**
 	 * Sets a column to a long value.
-	 * 
+	 *
 	 * @param column
-	 *            The name of the column.
+	 * 		The name of the column.
 	 * @param value
-	 *            The value to set.
+	 * 		The value to set.
 	 */
 	public void set(final String column, final long value) {
 		// TODO Store as a number for formatting
@@ -254,11 +252,11 @@ public class FlatFileRow {
 
 	/**
 	 * Sets a column to a big decimal value.
-	 * 
+	 *
 	 * @param column
-	 *            The name of the column.
+	 * 		The name of the column.
 	 * @param value
-	 *            The value to set.
+	 * 		The value to set.
 	 */
 	public void set(final String column, final BigDecimal value) {
 		// TODO Store as a number for formatting
@@ -267,11 +265,11 @@ public class FlatFileRow {
 
 	/**
 	 * Sets a column to a string value.
-	 * 
+	 *
 	 * @param column
-	 *            The name of the column.
+	 * 		The name of the column.
 	 * @param value
-	 *            The value to set.
+	 * 		The value to set.
 	 */
 	public void set(final String column, final String value) {
 		final FlatFileColumn flatFileColumn = this.columns.get(column);
@@ -280,6 +278,18 @@ public class FlatFileRow {
 		} else {
 			throw new IllegalArgumentException("Invalid column name: " + column);
 		}
+	}
+
+	/**
+	 * Checks if the value in the specified column is null or empty.
+	 *
+	 * @param column
+	 * 		The column of the row to check.
+	 * @return true if the value in the specified column is null or empty, otherwise false.
+	 * @see StringUtils#isBlank(String)
+	 */
+	public boolean isBlank(String column) {
+		return StringUtils.isBlank(get(column));
 	}
 
 }
